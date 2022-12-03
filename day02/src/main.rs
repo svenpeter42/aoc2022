edef52852339c2d83987a9dbe44e9acf53e69f80
Sven Peter <sven@svenpeter.dev>
2022-12-03 22:17:32 +0100

    day02: use Enums

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,89 +1,121 @@
+#![warn(clippy::pedantic)]
+
 use std::error::Error;
 use std::fs;
+use std::str::FromStr;
 
-const ROCK: u32 = 1;
-const PAPER: u32 = 2;
-const SCISSORS: u32 = 3;
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
 
-const LOOSE: u32 = 1;
-const DRAW: u32 = 2;
-const WIN: u32 = 3;
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Goal {
+    Loose,
+    Draw,
+    Win,
+}
 
-fn parse_move(x: &str) -> Result<u32, &'static str> {
-    match x {
-        "A" => Ok(ROCK),
-        "B" => Ok(PAPER),
-        "C" => Ok(SCISSORS),
-        "X" => Ok(ROCK),
-        "Y" => Ok(PAPER),
-        "Z" => Ok(SCISSORS),
-        _ => Err("Invalid move"),
+impl FromStr for Move {
+    type Err = &'static str;
+
+    fn from_str(input: &str) -> Result<Move, Self::Err> {
+        match input {
+            "A" => Ok(Move::Rock),
+            "B" => Ok(Move::Paper),
+            "C" => Ok(Move::Scissors),
+            "X" => Ok(Move::Rock),
+            "Y" => Ok(Move::Paper),
+            "Z" => Ok(Move::Scissors),
+            _ => Err("Invalid move"),
+        }
     }
 }
 
-fn parse_goal(x: &str) -> Result<u32, &'static str> {
-    match x {
-        "X" => Ok(LOOSE),
-        "Y" => Ok(DRAW),
-        "Z" => Ok(WIN),
-        _ => Err("Invalid goal"),
+impl FromStr for Goal {
+    type Err = &'static str;
+
+    fn from_str(input: &str) -> Result<Goal, Self::Err> {
+        match input {
+            "X" => Ok(Goal::Loose),
+            "Y" => Ok(Goal::Draw),
+            "Z" => Ok(Goal::Win),
+            _ => Err("Invalid goal"),
+        }
     }
 }
 
-fn calc_score(x: u32, y: u32) -> Result<u32, &'static str> {
+fn calc_winloss_score(x: Move, y: Move) -> usize {
     match (x, y) {
-        (ROCK, ROCK) => Ok(3),
-        (PAPER, PAPER) => Ok(3),
-        (SCISSORS, SCISSORS) => Ok(3),
-        (ROCK, PAPER) => Ok(6),
-        (PAPER, ROCK) => Ok(0),
-        (ROCK, SCISSORS) => Ok(0),
-        (SCISSORS, ROCK) => Ok(6),
-        (PAPER, SCISSORS) => Ok(6),
-        (SCISSORS, PAPER) => Ok(0),
-        _ => Err("Invalid moves to calc score"),
+        (Move::Rock, Move::Rock) => 3,
+        (Move::Paper, Move::Paper) => 3,
+        (Move::Scissors, Move::Scissors) => 3,
+        (Move::Rock, Move::Paper) => 6,
+        (Move::Paper, Move::Rock) => 0,
+        (Move::Rock, Move::Scissors) => 0,
+        (Move::Scissors, Move::Rock) => 6,
+        (Move::Paper, Move::Scissors) => 6,
+        (Move::Scissors, Move::Paper) => 0,
     }
 }
 
-fn get_move(x: u32, goal: u32) -> Result<u32, &'static str> {
-    match (x, goal) {
-        (WIN, ROCK) => Ok(PAPER),
-        (WIN, PAPER) => Ok(SCISSORS),
-        (WIN, SCISSORS) => Ok(ROCK),
-        (DRAW, ROCK) => Ok(ROCK),
-        (DRAW, PAPER) => Ok(PAPER),
-        (DRAW, SCISSORS) => Ok(SCISSORS),
-        (LOOSE, ROCK) => Ok(SCISSORS),
-        (LOOSE, PAPER) => Ok(ROCK),
-        (LOOSE, SCISSORS) => Ok(PAPER),
-        _ => Err("Invalid inputs to get move"),
+fn calc_score(x: Move) -> usize {
+    match x {
+        Move::Rock => 1,
+        Move::Paper => 2,
+        Move::Scissors => 3,
+    }
+}
+
+fn get_winning_move(x: Move) -> Move {
+    match x {
+        Move::Rock => Move::Paper,
+        Move::Paper => Move::Scissors,
+        Move::Scissors => Move::Rock,
+    }
+}
+
+fn get_loosing_move(x: Move) -> Move {
+    match x {
+        Move::Rock => Move::Scissors,
+        Move::Paper => Move::Rock,
+        Move::Scissors => Move::Paper,
+    }
+}
+
+fn get_move(x: Move, goal: Goal) -> Move {
+    match goal {
+        Goal::Win => get_winning_move(x),
+        Goal::Draw => x,
+        Goal::Loose => get_loosing_move(x),
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let score: u32 = fs::read_to_string("input.txt")?
+    let score: usize = fs::read_to_string("input.txt")?
         .lines()
         .map(|line| {
             let moves = line.split(" ").collect::<Vec<&str>>();
-            let x = parse_move(moves[0]).unwrap();
-            let y = parse_move(moves[1]).unwrap();
-            let result_score = calc_score(x, y).unwrap();
-            result_score + y
+            let x = Move::from_str(moves[0]).unwrap();
+            let y = Move::from_str(moves[1]).unwrap();
+            calc_score(y) + calc_winloss_score(x, y)
         })
         .sum();
     println!("A: {score}");
 
-    let score2: u32 = fs::read_to_string("input.txt")?
+    let score2: usize = fs::read_to_string("input.txt")?
         .lines()
         .map(|line| {
             let moves = line.split(" ").collect::<Vec<&str>>();
-            let x = parse_move(moves[0]).unwrap();
-            let goal = parse_goal(moves[1]).unwrap();
-            let y = get_move(x, goal).unwrap();
-            let result_score = calc_score(x, y).unwrap();
-            result_score + y
+            let x = Move::from_str(moves[0]).unwrap();
+            let goal = Goal::from_str(moves[1]).unwrap();
+            let y = get_move(x, goal);
+            calc_score(y) + calc_winloss_score(x, y)
         })
         .sum();
     println!("B: {score2}");
+
     Ok(())
 }

9412bdbc1e9571e4a803e5dbd78c9d51250c72a7
Sven Peter <sven@svenpeter.dev>
2022-12-04 12:14:23 +0100

    day04

---
+++ day04/src/main.rs
@@ -0,0 +1,47 @@
+#![warn(clippy::pedantic)]
+
+use itertools::Itertools;
+use std::error::Error;
+use std::fs;
+
+trait Interval<Rhs = Self> {
+    fn contains(self, rhs: Rhs) -> bool;
+    fn overlaps(self, rhg: Rhs) -> bool;
+}
+
+impl Interval for (u32, u32) {
+    fn contains(self, other: (u32, u32)) -> bool {
+        self.0 <= other.0 && self.1 >= other.1
+    }
+
+    fn overlaps(self, other: (u32, u32)) -> bool {
+        (self.0 <= other.0 && self.1 >= other.0)
+            || (self.1 >= other.0 && self.1 <= other.1)
+            || (self.0 <= other.1 && self.1 >= other.1)
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let score = fs::read_to_string("input.txt")?
+        .lines()
+        .map(|line| {
+            let elf_interval: ((u32, u32), (u32, u32)) = line
+                .split(",")
+                .map(|elf| {
+                    elf.split("-")
+                        .map(|i| i.parse::<u32>().unwrap())
+                        .next_tuple()
+                        .unwrap()
+                })
+                .next_tuple()
+                .unwrap();
+            (
+                (elf_interval.0.contains(elf_interval.1) || elf_interval.1.contains(elf_interval.0))
+                    as u32,
+                (elf_interval.0.overlaps(elf_interval.1)) as u32,
+            )
+        })
+        .fold((0, 0), |a, b| (a.0 + b.0, a.1 + b.1));
+    println!("{score:?}");
+    Ok(())
+}
